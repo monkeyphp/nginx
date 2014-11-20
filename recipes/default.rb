a738ed5030a8dc8de47c64cbc5b696f73b310db5
David White <david@monkeyphp.com>
2014-11-20 15:42:05 +0000

    Removed default.conf

--- recipes/default.rb
+++ recipes/default.rb
@@ -61,10 +61,10 @@ when "centos"
         enable node['nginx']['default_site_enabled']
     end
     
-    # file "/etc/nginx/conf.d/default.conf" do
-    #     action :delete
-    #     notifies :start, resources(:service => "nginx")
-    # end
+    file "/etc/nginx/conf.d/default.conf" do
+        action :delete
+        notifies :start, resources(:service => "nginx")
+    end
 
     # wget http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
     # rpm -ivh nginx-release-centos-6-0.el6.ngx.noarch.rpm
